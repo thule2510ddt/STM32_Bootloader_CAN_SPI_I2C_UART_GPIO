2a0604e847680047ba06037274befbc879e7a436
thule2510ddt <lmthu.2510@gmail.com>
2024-10-19 23:32:42 +0700

    first commit

---
+++ Drivers/stm32f4xx_drivers/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h \
+ H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx_gpio_driver.h \
+ H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h
+H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h:
+H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx_gpio_driver.h:
+H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h:
---
+++ Drivers/stm32f4xx_drivers/Debug/drivers/Src/stm32f411xx_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f411xx_gpio_driver.o: \
+ ../drivers/Src/stm32f411xx_gpio_driver.c \
+ H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx_gpio_driver.h \
+ H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h \
+ H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx_gpio_driver.h
+H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx_gpio_driver.h:
+H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h:
+H:/STM32_Bootloader_CAN_SPI_I2C_UART_GPIO/Drivers/stm32f4xx_drivers/drivers/Inc/stm32f411xx_gpio_driver.h:
